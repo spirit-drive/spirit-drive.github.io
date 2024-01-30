571983ce05e67f5ff345bd8d61a8d3e741c6ee4f
Igor Zvyagin <spirit-drive@yandex.ru>
2024-01-30 09:42:40 +0300

    feat: made a hover on tip

--- src/components/Tip/Tip.stories.tsx
+++ src/components/Tip/Tip.stories.tsx
@@ -7,8 +7,8 @@ import React from 'react';
 const Wrapper = () => (
   <ComponentInfo title="Tip" desc={`Компонент использует useLayoutEffect и onTransitionEnd.`}>
     <div style={{ position: 'relative', width: 400, height: 1200, overflow: 'hidden' }}>
-      <Tip title="ttile1">
-        <div style={{ height: 40, top: 60, left: 10, backgroundColor: '#efc' }}>test</div>
+      <Tip title="Текст получил широкое распространение в учебных и демонстрационных материалах по полиграфии и дизайну печатных изданий и веб-страниц. Распространился в 1970-х годах из-за трафаретов компании Letraset, а затем — из-за того, что служил примером текста в программе PageMaker. Применяется для того, чтобы показать читателю (пользователю) как будет выглядеть страница при использовании того или иного набора оформительских элементов и шрифтов. Бессмысленность текста приводит к тому, что при этом читатель сосредотачивается именно на внешнем виде и общем восприятии страницы, а не на ее содержании">
+        <div style={{ height: 50, top: 60, left: 10, backgroundColor: '#efc' }}>test</div>
       </Tip>
       <Tip title="ttile1">
         <span style={{ position: 'absolute', width: 40, height: 40, top: 60, left: 10, backgroundColor: '#efc' }}>
@@ -17,7 +17,7 @@ const Wrapper = () => (
         {/*<div></div>*/}
       </Tip>
       <Tip title="ttile1">
-        <div style={{ position: 'absolute', width: 40, height: 40, top: 60, left: 80, backgroundColor: '#efc' }}></div>
+        <div style={{ position: 'absolute', width: 40, height: 20, top: 60, left: 80, backgroundColor: '#efc' }}></div>
       </Tip>
       <Tip title="ttile1">
         <div style={{ position: 'absolute', width: 40, height: 40, top: 60, left: 160, backgroundColor: '#efc' }}>
@@ -25,7 +25,7 @@ const Wrapper = () => (
         </div>
       </Tip>
       <Tip title="ttile1">
-        <div style={{ position: 'absolute', width: 40, height: 40, top: 60, left: 240, backgroundColor: '#efc' }}>
+        <div style={{ position: 'absolute', width: 40, height: 80, top: 60, left: 240, backgroundColor: '#efc' }}>
           <div>
             <div>2323</div>
           </div>
--- src/components/Tip/Tip.tsx
+++ src/components/Tip/Tip.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactFragment, useEffect, useLayoutEffect, useReducer, useRef, useState } from 'react';
+import React, { FC, useEffect, useLayoutEffect, useReducer, useRef, useState } from 'react';
 import cn from 'clsx';
 import s from './Tip.module.sass';
 import { createPortal } from 'react-dom';
@@ -67,9 +67,15 @@ export type TipPosition = {
   left: number;
 };
 
+export enum TipPlace {
+  top = 'top',
+  bottom = 'bottom',
+}
+
 export const Tip: FC<TipProps> = ({ className, children, title, container = document?.body }) => {
   const [state, dispatch] = useReducer(reducer, { mount: false, visible: false });
   const [position, setPosition] = useState<TipPosition>({ left: 0, top: 0 });
+  const [place, setPlace] = useState<TipPlace>(TipPlace.top);
   const tip = useRef<HTMLDivElement>();
   const holder = useRef<HTMLDivElement>();
 
@@ -82,7 +88,10 @@ export const Tip: FC<TipProps> = ({ className, children, title, container = docu
     className: null,
   });
 
+  const timeoutId = useRef<number>();
+
   const onMouseEnter = () => {
+    clearTimeout(timeoutId.current);
     dispatch({ type: TipType.mount });
     setTimeout(() => dispatch({ type: TipType.visible }), 0);
 
@@ -96,8 +105,10 @@ export const Tip: FC<TipProps> = ({ className, children, title, container = docu
   };
 
   const onMouseLeave = () => {
-    dispatch({ type: TipType.invisible });
-    setTimeout(() => dispatch({ type: TipType.unmount }), 0);
+    timeoutId.current = window?.setTimeout(() => {
+      dispatch({ type: TipType.invisible });
+      setTimeout(() => dispatch({ type: TipType.unmount }), 0);
+    }, 1000);
   };
 
   const parent = React.createElement(
@@ -106,11 +117,34 @@ export const Tip: FC<TipProps> = ({ className, children, title, container = docu
     clonedChild
   );
 
+  useLayoutEffect(() => {
+    if (tip.current && state.mount) {
+      const rect = tip.current.getBoundingClientRect();
+      const rectHolder = holder.current.getBoundingClientRect();
+      if (rect.top < 8) {
+        setPlace(TipPlace.bottom);
+        setPosition((v) => ({ ...v, top: v.top + rectHolder.height }));
+      } else {
+        setPlace(TipPlace.top);
+      }
+    }
+  }, [state.mount]);
+
+  useEffect(() => {
+    if (!state.mount) setPlace(TipPlace.top);
+  }, [state.mount]);
+
   return (
     <>
       {state.mount &&
         createPortal(
-          <div ref={tip} style={position} className={cn(s.root, state.visible && s.visible, className)}>
+          <div
+            ref={tip}
+            style={position}
+            onMouseEnter={() => clearTimeout(timeoutId.current)}
+            onMouseLeave={onMouseLeave}
+            className={cn(s.root, s[place], state.visible && s.visible, className)}
+          >
             {title}
           </div>,
           container
